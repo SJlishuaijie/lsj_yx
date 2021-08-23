7b18445ddffa62e974c524ed83476a00195aa72a
lsj <1851752509@qq.com>
2021-08-23 21:01:04 +0800

    第二次提交 V2.1

--- src/test/java/cn/baizhi/test/TetsGit.java
+++ src/test/java/cn/baizhi/test/TetsGit.java
@@ -1,4 +1,10 @@
 package cn.baizhi.test;
 
 public class TetsGit {
+
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbb");
+    }
+
 }
